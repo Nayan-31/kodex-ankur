b1a1ce939cdca28768687bc4f15ff3c8348ee7e5
ankurdotio <prajapatiankur8349@gmail.com>
2026-06-19 21:41:08 +0530

    feat: implement link creation API with authentication middleware

---
+++ linktree/backend/src/routes/link.routes.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import authMiddleware from '../middlewares/auth.middleware.js';
+import * as linkController from '../controllers/links.controller.js';
+
+const router = Router();
+
+router.post("/", authMiddleware, linkController.createLink)
+
+export default router;
